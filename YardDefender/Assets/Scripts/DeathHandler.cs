3ee1d9d0b11115b55c29be3a217be5b73cad0779
Erik <ErikOverflow@gmail.com>
2019-12-21 22:43:34 -0600

    Add death animation and disable enemy after animation

--- YardDefender/Assets/Scripts/DeathHandler.cs
+++ YardDefender/Assets/Scripts/DeathHandler.cs
@@ -6,18 +6,35 @@ public class DeathHandler : MonoBehaviour
 {
     HealthController healthController = null;
     MobStats mobStats = null;
+    AudioSource audioSource = null;
+    Animator animator = null;
     //Have an EnemyDetail script that has loot table and exp amount
 
     void Awake()
     {
         healthController = GetComponent<HealthController>();
         mobStats = GetComponent<MobStats>();
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         healthController.OnDeath += HandleDeath;
     }
 
     void HandleDeath()
     {
         ActiveGame.instance.IncreaseExperience(mobStats.Experience);
+        audioSource.Play();
+        animator.SetBool("Alive", false);
+        StartCoroutine(DisableAfterAnimation());
+        //gameObject.SetActive(false);
+    }
+
+    IEnumerator DisableAfterAnimation()
+    {
+        yield return null;
+        while (!animator.GetCurrentAnimatorStateInfo(0).IsName("ZombieState"))
+        {
+            yield return null;
+        }
         gameObject.SetActive(false);
     }
 
