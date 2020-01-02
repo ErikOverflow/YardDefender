d0aafe187b791372847f80bb1171731e92bb0af2
ErikOverflow <46797334+ErikOverflow@users.noreply.github.com>
2020-01-01 18:22:32 -0600

    Set up functional inventory UI

---
+++ YardDefender/Assets/Scripts/UILogic/UIWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+    [SerializeField] Animator animator = null;
+
+    public void ToggleWindow()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            CloseWindow();
+        }
+        else
+        {
+            OpenWindow();
+        }
+    }
+
+    private void OpenWindow()
+    {
+        StopCoroutine(WaitForAnimationFinishBeforeInactivate());
+        gameObject.SetActive(true);
+        animator.SetTrigger("Open");
+    }
+
+    private void CloseWindow()
+    {
+        animator.SetTrigger("Close");
+        StartCoroutine(WaitForAnimationFinishBeforeInactivate());
+    }
+
+    IEnumerator WaitForAnimationFinishBeforeInactivate()
+    {
+        yield return null;
+        while(animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
