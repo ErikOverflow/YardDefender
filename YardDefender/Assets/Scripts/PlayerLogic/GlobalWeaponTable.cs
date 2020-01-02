f111c4629286a3efb20e2af76704e7a15ba3d067
Erik <ErikOverflow@gmail.com>
2020-01-01 22:26:19 -0600

    Fix line endings

--- YardDefender/Assets/Scripts/PlayerLogic/GlobalWeaponTable.cs
+++ YardDefender/Assets/Scripts/PlayerLogic/GlobalWeaponTable.cs
@@ -1,10 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Weapon Table", menuName = "Create New Weapon Table", order = 1)]
-public class GlobalWeaponTable : ScriptableObject
-{
-    public Weapon[] weapons;
-}
-
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;[CreateAssetMenu(fileName = "New Weapon Table", menuName = "Create New Weapon Table", order = 1)]public class GlobalWeaponTable : ScriptableObject{    public Weapon[] weapons;}
